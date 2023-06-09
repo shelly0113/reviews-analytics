ce4a68088e2f9a06b6c0a6f972d6015aa463f840
Xinyi Jiang <csy50114@gmail.com>
2023-06-09 23:29:11 +0800

    add wc count

--- Read.py
+++ Read.py
@@ -8,6 +8,8 @@ with open('reviews.txt', 'r') as f:
             print(len(data))
 print('檔案讀取完了，總共有', len(data), '筆資料')
 
+print(data[0])
+
 sum_len = 0
 for d in data:
     sum_len = sum_len + len(d)
@@ -20,3 +22,37 @@ for d in data:
         new.append(d)
 print('一共有', len(new), '比留言長度小於100')
 print(new[0])
+
+good = []
+for d in data:
+    if 'good' in d:
+        good.append(d)
+print('一共有', len(good), '筆資料提到good')
+print(good[0])
+
+# 文字計數
+wc = {}  # word_count
+for d in data:
+    words = d.split()
+    for word in words:
+        if word in wc:
+            wc[word] += 1
+        else:
+            wc[word] = 1
+
+for word in wc:  # 把字典的key找出來
+    if wc[word] > 10000:
+        print(word, wc[word])
+
+print('總數量: ', len(wc))
+
+
+while True:
+    word = input('請問你想查什麼字: ')
+    if word == 'q':
+        break
+    if word in wc:
+        print(word, '出現過的次數為: ', wc[word])
+    else:
+        print('這個字沒有出現過喔!')
+print('感謝使用本查詢功能')
